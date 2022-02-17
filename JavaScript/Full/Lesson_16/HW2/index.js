1e3ceb101ae1c22dd4a78bff9b299d28e197e2c4
renbak <renbak.ua@gmail.com>
2022-02-17 19:36:44 +0200

    brain_teaser

---
+++ JavaScript/Full/Lesson_16/HW2/index.js
@@ -0,0 +1,32 @@
+/* eslint-disable */
+
+/* Начальный код для этой задачи написан с плохими практиками, такими как использования var, 
+переопределение переменной и тд. Это сделано намерено чтобы разобрать работу var.
+Чтобы eslint не ругался на ошибки, для этой задачи он отключен аннотацией eslint-disable */
+
+// var foo = 1;
+
+// function bar() {
+//   if (!foo) {
+//     var foo = 10;
+//   }
+
+//   return foo;
+// }
+
+// var foo = bar();
+
+// ********************************
+
+let foo = 1;
+
+function bar() {
+  if (!foo) {
+    let foo = 10;
+  }
+  console.log(foo);
+  return foo;
+}
+
+// var
+export default foo = bar();
